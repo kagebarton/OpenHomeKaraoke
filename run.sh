12819de4f9e5bea2e16f129ec299cd0700885f2e
kagebarton <ktstuff45@hotmail.com>
2026-03-15 16:30:04 -0400

    Startup and exit fixes
    Fixed video window not popping up when used with discrete GPU
    Fixed tmux session leaving a pane unused
    Fixed esc key not cleaning up the tmux session

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 session_name=PiKaraoke
-cmds=("top"
+cmds=(
+#"top"
 #"sudo sh -c 'cp -f $HOME/.Xauthority ~ && PATH=$PATH python3 app.py -u $(whoami)'"
-"PATH='$PATH' python3 app.py"
-"PATH='$PATH' ./screencapture.sh -v -p 4000"
-"PATH='$PATH' python3 vocal_splitter.py -p -d ~/pikaraoke-songs/"
+#"PATH='$PATH' ./screencapture.sh -v -p 4000"
+"python3 vocal_splitter.py -d ~/pikaraoke-songs/ -rd /mnt/ramdisk"
+"DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY SDL_VIDEODRIVER=x11 python3 app.py --show-overlay -w -nv --admin-password admin; tmux kill-session -t $session_name"
 #"pavucontrol"
 )
 
@@ -18,24 +19,28 @@ export DISPLAY=:0
 
 cd "`dirname $0`"
 tmux new-session -s $session_name -d -x 240 -y 60
+tmux set-environment -t $session_name DISPLAY "$DISPLAY"
+tmux set-environment -t $session_name XAUTHORITY "$XAUTHORITY"
 
 for i in `seq 0 $[${#cmds[*]}-1]`; do
-	sleep 0.2
-	tmux split-window
-	sleep 0.2
-	tmux select-layout tile
-	sleep 0.2
+	if [ $i -gt 0 ]; then
+		sleep 0.2
+		tmux split-window
+		sleep 0.2
+		tmux select-layout even-horizontal
+		sleep 0.2
+	fi
 	tmux send-keys -l "${cmds[i]}"
 	sleep 0.2
 	tmux send-keys Enter
 done
 
 # Set pulseaudio recording source
-src="`pacmd list-sources | grep '.monitor>' | awk '{print $2}' | head -1 `"
-idx="`pacmd list-source-outputs | grep index: | awk '{print $2}' | tail -1`"
-if [ "$src" ]; then
-	pacmd move-source-output $idx "${src:1:-1}"
-fi
+#src="`pacmd list-sources | grep '.monitor>' | awk '{print $2}' | head -1 `"
+#idx="`pacmd list-source-outputs | grep index: | awk '{print $2}' | tail -1`"
+#if [ "$src" ]; then
+#	pacmd move-source-output $idx "${src:1:-1}"
+#fi
 
 tmux a -t $session_name
 
